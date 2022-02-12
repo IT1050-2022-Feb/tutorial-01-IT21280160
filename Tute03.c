b625c94f037c768ec161ddbf6ae2928810280633
it21280160 Theekshana <99546709+IT21280160@users.noreply.github.com>
2022-02-12 16:55:48 +0000

    Tute 01, 02, 03, 04

--- Tute03.c
+++ Tute03.c
@@ -13,7 +13,20 @@ sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
 int main() {
-  
+
+int num, sum = 0, x = 1;
+
+printf("Enter positive integer : ");
+scanf("%d", &num);
+
+while (x <= num)
+{
+  sum = sum + x;
+  x++;
+}
+
+printf("\nsummation of the numbers from 1 to %d = %d", num, sum);
+
   return 0;
 }
 
