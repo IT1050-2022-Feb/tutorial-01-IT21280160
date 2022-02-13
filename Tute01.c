e3e376fd32e90b181caf48f029db0bf20bafc9e9
it21280160 Theekshana <99546709+IT21280160@users.noreply.github.com>
2022-02-13 14:34:46 +0000

    Malabe

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,20 @@
 #include <stdio.h>
 
 int main() {
-  
-  return 0;
+
+int marks1, marks2, total;
+float avg;
+
+printf("\nEnter mark 1 : ");
+scanf("%d", &marks1);
+printf("Enter mark 2 : ");
+scanf("%d", &marks2);
+
+total = marks1 + marks2;
+
+avg = total / 2;
+
+printf("Average is : %.1f ", avg); 
+return 0;
 }
 
--- Tute02.c
+++ Tute02.c
@@ -19,6 +19,23 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 #include <stdio.h>
 
 int main() {
-  
+
+float distance, amount;
+
+printf("\nEnter the distance the van has travelled : ");
+scanf("%f", &distance);
+
+if (distance < 30 )
+{
+  amount = distance * 50;
+
+  printf("Amount is : %.2f ", amount);
+}  
+else 
+{
+  amount = 30 * 50 + (distance - 30) * 40;
+
+  printf("Amount is : %.2f ", amount);
+} 
   return 0;
 }
